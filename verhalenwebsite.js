be1fc8cce9e176ec20c07ecc2b4bc659f71b6b3e
elisatimmerman <45420467+elisatimmerman@users.noreply.github.com>
2019-01-12 17:42:46 +0100

    micro interactie

--- verhalenwebsite.js
+++ verhalenwebsite.js
@@ -2,9 +2,47 @@
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
 
-var zoekveld = document.querySelector('body > section > form > fieldset');
-var zoekbutton = document.querySelector('body > section > button');
 
-zoekbutton.addEventListener ('click', function(){
-zoekveld.classList.toggle('show-search');
-});
+var beoordeling = document.querySelector('.beoordeling');
+var knopBeoordeling = document.querySelector('.beoordelingKnop');
+
+if (knopBeoordeling != null){
+    knopBeoordeling.onclick = function(){showBeoordeling();};
+}
+
+
+
+
+
+var verstuurKnop = document.querySelector('.beoordelingVersturen');
+
+if (verstuurKnop != null){
+    verstuurKnop.onclick = function(e){
+    showBeoordeling();
+    verstuurd();
+}
+}
+
+
+function verstuurd(){   knopBeoordeling.classList.toggle('beoordelingKnopSpin');
+    console.log("Henk");
+}
+
+
+
+
+function showBeoordeling(){                beoordeling.classList.toggle('beoordelingZichtbaar');
+}
+
+var filterForm = document.querySelector('.filter');
+var filterButton = document.querySelector('button');
+
+if (filterButton != null){
+filterButton.addEventListener('click', show);
+}
+
+
+function show(){
+  filterForm.classList.toggle('zichtbaar');
+}
+
